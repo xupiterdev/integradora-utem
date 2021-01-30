d5ea15acdb2107b2cda16afc299bb4c90bf80bd5
Jose Cruz <hello.xupiter@gmail.com>
2021-01-29 20:33:53 -0600

    primera practica

--- App.js
+++ App.js
@@ -1,21 +1,68 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { 
+  StyleSheet, 
+  Text, 
+  View, 
+  Button,
+  TouchableOpacity
+} from 'react-native';
 
-export default function App() {
+
+export default function (){
+  const [contador, setContador] = useState(0);
+
+  const llegarA10 = () => {
+    setContador(contador+1)
+    if(contador === 10) {
+      alert("Hemos llegado a 10 cliks")
+      setContador(0);
+    }
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={Style.contenedor}>
+      <Text style={Style.title}>Proyecto integrador 5-TTI-1</Text>
+      <Text>Haz dado {contador} cliks!</Text>
+      <Button 
+        title="Mas 1"
+        color="#00C6C9"
+        onPress={llegarA10}
+      />
+
+      <TouchableOpacity
+        style={Style.btnRed}
+        onPress={() => setContador(contador-1)}
+      >
+        <Text style={Style.textWhite}>Menos 1</Text>
+      </TouchableOpacity>
+
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const Style = StyleSheet.create({
+  contenedor : {
+    flex : 1,
+    justifyContent : "center",
+    alignItems: "center"
   },
-});
+  title : {
+    fontSize : 20
+  },
+  btnRed : {
+    width: 300,
+    height : 100,
+    backgroundColor : "#B6151C",
+    borderRadius : 10,
+    justifyContent : "center",
+    alignItems: "center"
+  },
+  textWhite : {
+    color: "#f6f6f6",
+    fontSize:40
+  }
+})
+
+
+
+
